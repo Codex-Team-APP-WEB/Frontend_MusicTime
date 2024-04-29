75a8cb49a12c831e4f4bb7170b3ecfb38a665f89
Victor Raul Herrera Castillo <89873215+VictorHerrera10@users.noreply.github.com>
2024-04-29 16:19:04 -0500

    Create project and server.

---
+++ src/public/components/footer.component.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "footer.component"
+}
+</script>
+
+<template>
+  <footer>
+    <p>&copy; 2021 ACME Learning Center</p>
+  </footer>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/public/components/toolbar.component.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "toolbar.component"
+}
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
