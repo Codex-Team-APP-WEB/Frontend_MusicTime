f64db2b976e53bd66bad9184a1b3244dc53d4e35
Victor Herrera <89873215+VictorHerrera10@users.noreply.github.com>
2024-05-02 16:04:06 -0500

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import HomeComponent from "../public/pages/home.component.vue";
 import AboutComponent from "../public/pages/about.component.vue";
 import MusicianManagementComponent from "../profiles/pages/musician-management.component.vue";
 import subscriptionManagementComponent from "../subscription/pages/subscription-management.component.vue";
+import MusicianViewComponent from "../profiles/pages/musician-view.component.vue";
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/home",    component: HomeComponent, meta: { title: "Home"}},
         { path: "/about",   component: AboutComponent, meta: { title: "About us"}},
-        { path: '/Musician', component: MusicianManagementComponent, meta: { title: 'Musician' } },
+        { path: '/Management', component: MusicianManagementComponent, meta: { title: 'Management' } },
+        { path: '/Musician', component: MusicianViewComponent, meta: { title: 'Musician' } },
         { path: "/",        redirect: "/home"},
         { path: '/Subscription', component: subscriptionManagementComponent, meta: { title: 'Subscription' } }
     ]
