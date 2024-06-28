8b50f0c6563971c8af7672c85954449b5b7e59a2
Carlos Pingus <132234605+KazutoTR@users.noreply.github.com>
2024-06-28 12:55:04 -0500

    chore: Added About Page

--- src/public/pages/about.component.vue
+++ src/public/pages/about.component.vue
@@ -1,7 +1,10 @@
-<template>
-
-</template>
-
 <script>
-
+export default {
+  name: "about"
+}
 </script>
+
+<template>
+  <h1>About us</h1>
+  <p>Music Time is an Music Promotion Platform, part of the TempSound startup.</p>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,7 @@ import paymentGatewayComponent from "../subscription/pages/payment-gateway.compo
 import ContractPanelComponent from "../contract/pages/contract-panel.component.vue";
 import ContractCreationComponent from "../contract/pages/contract-creation.component.vue";
 import PageNotFoundComponent from "../public/pages/page-not-found.component.vue";
-import CostumerEditProfileComponent from "../profiles/pages/costumer-edit-profile.component.vue";
+import CustomerEditProfileComponent from "../profiles/pages/customer-edit-profile.component.vue";
 import MusicEditProfileComponent from "../profiles/pages/music-edit-profile.component.vue";
 import EnterpriseEditProfileComponent from "../profiles/pages/enterprise-edit-profile.component.vue";
 import SignInComponent from "../IAM/pages/sign-in.component.vue";
@@ -21,24 +21,24 @@ import CreateMusicianComponent from "../musician/pages/create-musician-profile.c
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: "/home",    component: HomeComponent, meta: { title: "Home"}},
-        { path: "/about",   component: AboutComponent, meta: { title: "About us"}},
-        { path: '/management', component: MusicianManagementComponent, meta: { title: 'Management' } },
-        { path: "/",        redirect: "/home"},
-        { path: '/subscription', component: subscriptionManagementComponent, meta: { title: 'Subscription' } },
-        { path: '/contract-panel', component: ContractPanelComponent, meta: { title: 'Contract Panel' } },
-        { path: '/costumer-edit', name: 'costumer-edit-profile', component: CostumerEditProfileComponent, meta: { title: 'Costumer Edit Profile' } },
-        { path: '/music-edit', name: 'music-edit-profile', component: MusicEditProfileComponent, meta: { title: 'Music Edit Profile' } },
+        { path: "/home",            component: HomeComponent, meta: { title: "Home"}},
+        { path: "/about",           component: AboutComponent, meta: { title: "About us"}},
+        { path: '/management',      component: MusicianManagementComponent, meta: { title: 'Management' } },
+        { path: "/",                redirect: "/home"},
+        { path: '/subscription',    component: subscriptionManagementComponent, meta: { title: 'Subscription' } },
+        { path: '/contract-panel',  component: ContractPanelComponent, meta: { title: 'Contract Panel' } },
+        { path: '/customer-edit',   name: 'customer-edit-profile', component: CustomerEditProfileComponent, meta: { title: 'Customer Edit Profile' } },
+        { path: '/music-edit',      name: 'music-edit-profile', component: MusicEditProfileComponent, meta: { title: 'Music Edit Profile' } },
         { path: '/enterprise-edit', name: 'enterprise-edit-profile', component: EnterpriseEditProfileComponent, meta: { title: 'Enterprise Edit Profile' } },
 
         //Path con boton no es de toolbar
-        { path: '/musician/:id', component: Musician, meta: { title: 'Musician' }, props: true },
-        { path: '/contract/:id', component: ContractCreationComponent, meta: { title: 'Contract' }, props: true },
-        { path: '/payment', component: paymentGatewayComponent, meta: { title: 'Payment' }  },
-        { path: '/contract-view/:id', component: ContractViewComponent, meta: { title: 'Contract View' }  },
+        { path: '/musician/:id',                component: Musician, meta: { title: 'Musician' }, props: true },
+        { path: '/contract/:id',                component: ContractCreationComponent, meta: { title: 'Contract' }, props: true },
+        { path: '/payment',                     component: paymentGatewayComponent, meta: { title: 'Payment' }  },
+        { path: '/contract-view/:id',           component: ContractViewComponent, meta: { title: 'Contract View' }  },
         { path: '/sign-in', name: 'sign-in',    component: SignInComponent,             meta: { title: 'Sign In' } },
         { path: '/sign-up', name: 'sign-up',    component: SignUpComponent,             meta: { title: 'Sign Up' } },
-        {path: '/create-musician', component: CreateMusicianComponent, meta: { title: 'Create Musician' }},
+        {path: '/create-musician',              component: CreateMusicianComponent, meta: { title: 'Create Musician' }},
 
         //Path page not found
         {path: '/:pathMatch(.*)*', name: 'not-found', component: PageNotFoundComponent }
